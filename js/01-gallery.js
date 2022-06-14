0d22a276c02d837010ca8bf2968f61f59d5d92f0
chekirko <victorche59@gmail.com>
2022-06-14 15:52:35 +0300

    створює та рендерить розмітку в задачі 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(arr) {
+  const markup = arr
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item"><img class="gallery__image" src="${preview}" alt="${description}"></div>`;
+    })
+    .join(' ');
+  return markup;
+}
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = galleryItemsMarkup;
+
+console.log(galleryItemsMarkup);
+
+// console.log(galleryItems);

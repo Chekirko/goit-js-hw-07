e84e3d764ee2f3c8103d12603d5a08138eacd39e
chekirko <victorche59@gmail.com>
2022-06-26 23:14:12 +0300

    додає рішення завдання 1 без Esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,5 @@
 import { galleryItems } from './gallery-items.js';
+// import * as basicLightbox from 'basiclightbox';
 // Change code below this line
 
 const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
@@ -15,6 +16,20 @@ function createGalleryItemsMarkup(arr) {
 const galleryEl = document.querySelector('.gallery');
 galleryEl.innerHTML = galleryItemsMarkup;
 
-console.log(galleryItemsMarkup);
+galleryEl.addEventListener('click', onImageClick);
+function onImageClick(event) {
+  const bigImage = galleryItems.find(item => item.description === event.target.getAttribute('alt'));
+  const instance = basicLightbox.create(`
+    <img src="${bigImage.original}" width="800" height="600">
+`);
+
+  instance.show();
+}
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `);
+
+// instance.show();
 
 // console.log(galleryItems);

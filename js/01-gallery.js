168f6768284f83a7fe20d862d6d3b7d117e4b4ce
chekirko <victorche59@gmail.com>
2022-07-03 01:37:00 +0300

    додаэ завдання 1 по тз ы завдання 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// import * as basicLightbox from 'basiclightbox';
-// Change code below this line
 
 const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+// Рішення по ТЗ
 
 function createGalleryItemsMarkup(arr) {
   const markup = arr
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item"><img class="gallery__image" src="${preview}" alt="${description}"></div>`;
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
     })
     .join(' ');
   return markup;
@@ -18,18 +27,44 @@ galleryEl.innerHTML = galleryItemsMarkup;
 
 galleryEl.addEventListener('click', onImageClick);
 function onImageClick(event) {
-  const bigImage = galleryItems.find(item => item.description === event.target.getAttribute('alt'));
+  event.preventDefault();
   const instance = basicLightbox.create(`
-    <img src="${bigImage.original}" width="800" height="600">
+    <img src="${event.target.dataset.source}" width="800" height="600">
 `);
-
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
   instance.show();
 }
 
-// const instance = basicLightbox.create(`
-//     <img src="assets/images/image.png" width="800" height="600">
+document.addEventListener('keydown', onModalEsc);
+
+function onModalEsc(event) {
+  if (event.code !== 'Esc') {
+    return;
+  }
+}
+
+// Рішення не по ТЗ
+
+// function createGalleryItemsMarkup(arr) {
+//   const markup = arr
+//     .map(({ preview, original, description }) => {
+//       return `<div class="gallery__item"><img class="gallery__image" src="${preview}" alt="${description}"></div>`;
+//     })
+//     .join(' ');
+//   return markup;
+// }
+
+// const galleryEl = document.querySelector('.gallery');
+// galleryEl.innerHTML = galleryItemsMarkup;
+
+// galleryEl.addEventListener('click', onImageClick);
+// function onImageClick(event) {
+//   const bigImage = galleryItems.find(item => item.description === event.target.getAttribute('alt'));
+//   const instance = basicLightbox.create(`
+//     <img src="${bigImage.original}" width="800" height="600">
 // `);
 
-// instance.show();
-
-// console.log(galleryItems);
+//   instance.show();
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(arr) {
+  const markup = arr
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>`;
+    })
+    .join(' ');
+  return markup;
+}
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = galleryItemsMarkup;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
